f265479c25ec975b42b30cb14b7f869432e38572
hanadajukki <lukanic2012@gmail.com>
2025-03-18 22:44:50 +0900

    KSH_3(오른쪽,왼쪽 애니메이션)
    player 오른쪽 왼쪽 애니메이터 구현했습니다.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 5f;
     public int attack;
 
     Animator moveAni;
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        
+        moveAni = GetComponent<Animator>();
     }
 
     
@@ -22,15 +22,15 @@ public class Player : MonoBehaviour
     {
         float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 
-        //if (Input.GetAxis("Horizontal") <= -0.5f)
-        //    moveAni.SetBool("left", true);
-        //else
-        //    moveAni.SetBool("left", false);
+        if (Input.GetAxis("Horizontal") <= -0.5f)
+            moveAni.SetBool("left", true);
+        else
+            moveAni.SetBool("left", false);
 
-        //if (Input.GetAxis("Horizontal") >= 0.5f)
-        //    moveAni.SetBool("right", true);
-        //else
-        //    moveAni.SetBool("right", false);
+        if (Input.GetAxis("Horizontal") >= 0.5f)
+            moveAni.SetBool("right", true);
+        else
+            moveAni.SetBool("right", false);
 
         transform.Translate(distanceX, 0, 0);
     }
