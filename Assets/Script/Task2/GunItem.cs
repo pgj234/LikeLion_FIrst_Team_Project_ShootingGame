67bd0ae82bf833de09a61cb031a45a72a3218933
박기락 <kirak0622@gmail.com>
2025-03-21 16:51:39 +0900

    PKR
    아트추가

--- Assets/Script/Task2/GunItem.cs
+++ Assets/Script/Task2/GunItem.cs
@@ -1,20 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GunItem : MonoBehaviour
 {
-
+    [SerializeField] private SpriteRenderer sr;
     [SerializeField] private TextMesh text;
+    [SerializeField] private TextMesh textShadow;
     [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private int textSortingOrder;
+    [SerializeField] private Color originColor = Color.white;
+    [SerializeField] private Color attackedColor = Color.red;
 
-    //data
     private int hp = 0;
-
-
+    private Coroutine colorCo = null;
 
     public void Init(int hp, float velocity)
     {
         this.hp = hp;
-        text.text = hp.ToString();
+        InitSprite();
+        InitText();
+        UpdateText();
         rb2d.linearVelocityY = -velocity;
 
         Destroy(gameObject, 20);//임시작업: 20초후 삭제.
@@ -28,6 +33,7 @@ public class GunItem : MonoBehaviour
 
             hp--;
             UpdateText();
+            ShowHitEffect();
             if (hp <= 0)
             {
                 //TODO. 플레이어 총기류 업그레이드
@@ -35,8 +41,50 @@ public class GunItem : MonoBehaviour
             }
         }
     }
+    void InitSprite()
+    {
+        sr.color = originColor;
+    }
+    void InitText()
+    {
+        SetSortingOrder(text.GetComponent<MeshRenderer>(), textSortingOrder);
+        SetSortingOrder(textShadow.GetComponent<MeshRenderer>(), textSortingOrder - 1);
+    }
+
+    void SetSortingOrder(Renderer render, int sortingOrder)
+    {
+        render.sortingLayerName = "Default";
+        render.sortingOrder = sortingOrder;
+    }
+
+    IEnumerator ChangeCo()
+    {
+        sr.color = attackedColor;
+        yield return new WaitForSeconds(0.1f);
+
+        float duration = 0.2f; // 원래 색으로 복귀하는 데 걸리는 시간
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            sr.color = Color.Lerp(attackedColor, originColor, t);
+            yield return null;
+        }
+
+        sr.color = originColor;
+    }
     void UpdateText()
     {
         text.text = hp.ToString();
+        textShadow.text = hp.ToString();
+    }
+    void ShowHitEffect()
+    {
+        if (colorCo != null)
+            StopCoroutine(colorCo);
+
+        StartCoroutine(ChangeCo());
     }
 }
