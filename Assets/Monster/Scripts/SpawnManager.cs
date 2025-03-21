f90fbddd34dc75ec7822455854614bcdb5de6b02
ChoiGyeongWon <ckw041333@naver.com>
2025-03-21 16:54:37 +0900

    CKW
    1. 스폰매니저 스크립트 수정
    
    1. Modify SpawnManager Script

--- Assets/Monster/Scripts/SpawnManager.cs
+++ Assets/Monster/Scripts/SpawnManager.cs
@@ -8,7 +8,9 @@ public class SpawnManager : MonoBehaviour
 	[SerializeField] float spawnMinX; // 몬스터 생성 최소 x좌표
 	[SerializeField] float spawnMaxX; // 몬스터 생성 최대 x좌표
 	[SerializeField] GameObject[] enemies; // 몬스터 객체 배열 (1단계 : 잡몹 / 2단계 : ~~ 등등)
-	float minDelay = 1f; // 몬스터 생성 최소 딜레이
+	[SerializeField] GameObject bossWarning; // 보스 등장 전 경고 이미지
+
+	float minDelay = 0.5f; // 몬스터 생성 최소 딜레이
 	float maxDelay = 3f; // 몬스터 생성 최대 딜레이
 
 	public int enemyIndex = 0; // 몬스터 인덱스
@@ -48,7 +50,7 @@ public class SpawnManager : MonoBehaviour
 			float randomX = Random.Range( spawnMinX, spawnMaxX );
 			yield return new WaitForSeconds( delay );
 			GameObject mon = Instantiate( enemies[enemyIndex], new Vector2( randomX, transform.position.y ), Quaternion.identity );
-			Destroy( mon, 4.5f );
+			Destroy( mon, 4f );
 			if (enemyCount >= 10 && enemyCount < 20)
 			{
 				enemyIndex = 1;
@@ -60,6 +62,9 @@ public class SpawnManager : MonoBehaviour
 			else if (enemyCount >= 30)
 			{
 				enemyIndex = 3;
+				bossWarning.SetActive( true );
+				yield return new WaitForSeconds( 3f );
+				bossWarning.SetActive( false );
 			}
 		}
 
