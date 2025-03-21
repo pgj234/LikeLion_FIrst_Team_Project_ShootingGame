407eb20068f9a4dc37968941ef5ba3e19ba7e93f
hanadajukki <lukanic2012@gmail.com>
2025-03-21 11:02:55 +0900

    KSH
    플레이어 애니메이션 제거
    플레이어 이미지 수정
    플레이어 뷰포트 클램프 구현

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,7 +5,9 @@ public class Player : MonoBehaviour
     public float speed = 5f;
     public int attack;
 
-    Animator moveAni;
+    //Animator moveAni;
+
+      
 
     public GameObject bullet;
     public Transform pos;
@@ -14,26 +16,33 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        moveAni = GetComponent<Animator>();
+       // moveAni = GetComponent<Animator>();
         InvokeRepeating("Shoot", 1f, 1f);
     }
 
     
     void Update()
     {
-        float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        if (Input.GetAxis("Horizontal") <= -0.5f)
-            moveAni.SetBool("left", true);
-        else
-            moveAni.SetBool("left", false);
+        
 
-        if (Input.GetAxis("Horizontal") >= 0.5f)
-            moveAni.SetBool("right", true);
-        else
-            moveAni.SetBool("right", false);
+        float distanceX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //if (Input.GetAxis("Horizontal") <= -0.5f)
+        //    moveAni.SetBool("left", true);
+        //else
+        //    moveAni.SetBool("left", false);
+
+        //if (Input.GetAxis("Horizontal") >= 0.5f)
+        //    moveAni.SetBool("right", true);
+        //else
+        //    moveAni.SetBool("right", false);
 
         transform.Translate(distanceX, 0, 0);
-        
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        viewPos.x = Mathf.Clamp01(viewPos.x);
+        viewPos.y = Mathf.Clamp01(viewPos.y);
+        Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos);
+        transform.position = worldPos;
     }
 
     
@@ -44,6 +53,8 @@ public class Player : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        
     }
 
     void Shoot()
