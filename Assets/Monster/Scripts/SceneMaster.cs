fdc4a07cc38fceec9c3b6f3448b59e4ed89ec5df
ChoiGyeongWon <ckw041333@naver.com>
2025-03-27 18:36:27 +0900

    CKW
    1. 게임시작씬 제작

---
+++ Assets/Monster/Scripts/SceneMaster.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMaster : MonoBehaviour
+{
+
+
+	public void OnClickStart()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void OnClickOut()
+	{
+		// 게임 종료 버튼 클릭 시 애플리케이션 종료
+		Application.Quit();
+
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;  // Unity 에디터에서 실행 중지
+#endif
+	}
+
+}
