766147e74fe2b0228b903f1e725eb50a8133719a
pgj234 <pgj94@naver.com>
2025-03-30 11:43:15 +0900

    PGJ
    1. 양 사이드 이미지로 가림
    2. 게임오버시 타임 스케일 = 0
    3. 플레이어 오브젝트 소트오더 1 설정

--- Assets/Script/GameOverUi.cs
+++ Assets/Script/GameOverUi.cs
@@ -3,11 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class GameOverUi : MonoBehaviour
 {
+    void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneMaster.instance.OnClickReStart();
+            Time.timeScale = 1;
         }
     }
 }
